8cbac1d615324d36e4f3e9a76bd4ad92ceb806f2
eomingyu <djalsrb123@naver.com>
2022-05-27 17:48:46 +0900

    예매 취소/취소 번호 조회 sql

--- show/src/main/java/com/mycompany/idev/mapper/TicketMapper.java
+++ show/src/main/java/com/mycompany/idev/mapper/TicketMapper.java
@@ -8,4 +8,6 @@ public interface TicketMapper {
 	void insertTicket(Ticket vo);
 	List<Ticket> getTicket(int ticket_no);
 	List<Ticket> getIdTicket(String id);
+	void cancelTicket(int ticket_no);
+	List<Ticket> getIdCancelledTicket(String id);
 }
